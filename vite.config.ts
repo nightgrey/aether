23ec7abd89568665220135b1b89bb76859ae42da
Nico <nightgrey@users.noreply.github.com>
2024-04-04 19:39:11 +0200

    add vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
+import { vercelPreset } from '@vercel/remix/vite';
 installGlobals();
 
 export default defineConfig({
   plugins: [
     remix({
+      presets: [vercelPreset()],
       ignoredRouteFiles: ['**/*.css'],
       future: {
         v3_fetcherPersist: true,
